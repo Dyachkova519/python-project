db6658ac8f5a61f19f1b347029652c3426bf67df
Dyachkova519 <72070056+Dyachkova519@users.noreply.github.com>
2021-03-31 19:12:37 +0300

    Update belaya_gvardiya.py

--- belaya_gvardiya.py
+++ belaya_gvardiya.py
@@ -1,17 +1,48 @@
+# -*- coding: utf-8 -*-
+
 import re
+
+p_dialog = re.compile(r'^вЂ” .*[.!?]$')
+p_speech = re.compile(r'[^.!?]*: В«.*В»\.')
+
 def speech_from_text(file):
     f = open(file, 'r', encoding= 'utf-8')
     speechlist = []
     for line in f:
-        resultdialog = re.findall(r'^— [^—]*[.!?]$', line)#вытаскивает реплики, которые оканчиваются точкой без авторских комментариев. но сюда не попадают реплики, врутри которых есть тире :(
-  #можно поменять регулярку на '^— .*[.!?]$', тогда ей вытаскиваются все (кажется) виды диалогов, в том числе с авторскими комментариями внутри. может, так даже лучше, но не очень понятно, как отделять зёрна от плевел
+        resultdialog = p_dialog.findall(line)
         if len(resultdialog) != 0:
             speechlist.extend(resultdialog)
-        resultspeech = re.findall(r'[^.!?]*: ".*"\.', line)
+        resultspeech = p_speech.findall(line)
         if len(resultspeech) != 0:
             speechlist.extend(resultspeech)
     f.close()
 
     return speechlist
+#for i in speech_from_text('belaya_gvardia.txt'):
+  #print(i)
+myli = speech_from_text('belaya_gvardia.txt')
 
-print(speech_from_text('belaya_gvardia.txt'))
+p_author2 = re.compile(r'[,.!?] вЂ” [^вЂ”.!?]*\. вЂ” ')
+p_author3 = re.compile(r', вЂ” [^вЂ”.!?]*, вЂ” ')
+p_speechandauthor1 = re.compile(r'^вЂ” [^вЂ”]*[,.] вЂ” [^вЂ”!?]*\.$')
+p_author1 = re.compile(r', вЂ” [^вЂ”!?]*\.')
+
+def author_com_from_dialog(dialoglist):
+  authors_comments = []
+  for dialog in dialoglist:
+    authorlist1 = p_speechandauthor1.findall(dialog)
+    if len(authorlist1) != 0:
+      for elem in authorlist1:
+        result = p_author1.findall(elem)
+        if len(result) != 0:
+            authors_comments.extend(result)
+    authorlist2 = p_author2.findall(dialog)
+    if len(authorlist2) != 0:
+        authors_comments.extend(authorlist2)
+    authorlist3 = p_author3.findall(dialog)
+    if len(authorlist3) != 0:
+        authors_comments.extend(authorlist3)
+      
+    return authors_comments
+
+print(author_com_from_dialog(myli))
