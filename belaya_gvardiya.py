7275034977885fd8892a07fd0dde7407ad4b86e6
Dyachkova519 <72070056+Dyachkova519@users.noreply.github.com>
2021-04-23 15:10:13 +0300

    Update belaya_gvardiya.py

--- belaya_gvardiya.py
+++ belaya_gvardiya.py
@@ -3,8 +3,10 @@
 import re
 import json
 
-p_dialog = re.compile(r'^— .*[.!?]$')
-p_speech = re.compile(r'[^.!?]*: «.*»\.')
+p_dialog = re.compile(r'^— .+[.!?]$')
+p_dialog_with_comm = re.compile(r'[.!?]*[^.!?]+:$\n^— .+[.!?]$', flags=re.MULTILINE)
+p_speech = re.compile(r'[^.!?]*: «[^»]*»')
+
 
 # -*- coding: utf-8 -*-
 
@@ -12,38 +14,50 @@ def speech_from_text(file):
     f = open(file, 'r', encoding= 'utf-8')
     allspeecheslist = []
     speechlist = []
+    previous_line = ''
     for line in f:
-        sample = line.strip()
-        flag = False
-        for elem in sample:
-          if elem in '1234567890':
+        if len(line) > 1:
+          two_lines = previous_line + line
+          sample = line.strip()
+          flag = False
+          for elem in sample:
+            if elem in '1234567890':
+              flag = True
+            else:
+              flag = False
+              break
+          if flag == True:
+            allspeecheslist.append(speechlist)
+            speechlist = []
+          resultdialog_with_comm = p_dialog_with_comm.findall(two_lines)
+          if len(resultdialog_with_comm) != 0:
+              speechlist.extend(resultdialog_with_comm)
+          resultdialog = p_dialog.findall(line)
+          if len(resultdialog) != 0:
             flag = True
-          else:
-            flag = False
-            break
-        if flag == True:
-          allspeecheslist.append(speechlist)
-          speechlist = []
-        resultdialog = p_dialog.findall(line)
-        if len(resultdialog) != 0:
-            speechlist.extend(resultdialog)
-        resultspeech = p_speech.findall(line)
-        if len(resultspeech) != 0:
-            speechlist.extend(resultspeech)
+            if len(resultdialog_with_comm) != 0:
+              for elem in resultdialog_with_comm:
+                if resultdialog[0] in elem:
+                  flag = False
+            if flag == True:
+              speechlist.extend(resultdialog)
+          resultspeech = p_speech.findall(line)
+          if len(resultspeech) != 0:
+              speechlist.extend(resultspeech)
+          previous_line = line
     allspeecheslist.append(speechlist)
     f.close()
     
     return allspeecheslist
-for i in speech_from_text('belaya_gvardia.txt'):
-  print(i)
 myli = speech_from_text('belaya_gvardia.txt')
-
-
 
 p_author2 = re.compile(r'^(— .+[,.!?]+) — ([^—]+\.) — (.+)$')
 p_author3 = re.compile(r'^(— .+[,.!?]+) — ([^—]+,) — (.+)$')
-p_speechandauthor1 = re.compile(r'^— ([^—]+[,.]) — ([^—]+\.)$')
+p_speechandauthor1 = re.compile(r'^— ([^—]+[,.?!]) — ([^—]+\.)$')
 p_author4 = re.compile(r'^(— [^—]+[.!?]+)$')
+p_multilineauthor = re.compile(r'[.!?]*([^.!?]+:)\n(— .+[.!?])$')
+p_speech_quotmarks = re.compile(r'([^.!?]*:) («[^»]*»)')
+
 
 
 def author_com_from_dialog(dialoglist):
@@ -51,6 +65,9 @@ def author_com_from_dialog(dialoglist):
   authors_comments = []
   for chapter in dialoglist:
     for dialog in chapter:
+      authorlist_multiline = p_multilineauthor.findall(dialog)
+      if len(authorlist_multiline) != 0:
+          authors_comments.extend(authorlist_multiline)
       authorlist1 = p_speechandauthor1.findall(dialog)
       if len(authorlist1) != 0:
           authors_comments.extend(authorlist1)
@@ -63,15 +80,17 @@ def author_com_from_dialog(dialoglist):
       authorlist4 = p_author4.findall(dialog)
       if len(authorlist4) != 0:
           authors_comments.extend(authorlist4)
+      quotspeechlist = p_speech_quotmarks.findall(dialog)
+      if len(quotspeechlist) != 0:
+          authors_comments.extend(quotspeechlist)
     all_author_comments.append(authors_comments)
     authors_comments = []
       
   return all_author_comments
 
-for i in author_com_from_dialog(myli):
-  print(i)
-
 delimited_speech = author_com_from_dialog(myli)
+
+
 def write_to_json(speech_and_comments):
   with open('results.json', 'w', encoding='utf-8') as f:
       jsonspeeches = {'chapter': 'all'}
@@ -87,14 +106,21 @@ def write_to_json(speech_and_comments):
             newdi['speech'] = elem
             newdi['author_text'] = None
           elif type(elem) == tuple and len(elem) == 2:
-            newdi['author'] = 'undefined'
-            newdi['speech'] = elem[0]
-            newdi['author_text'] = elem[-1]
+            if elem[0][-1] == ':':
+              newdi['author'] = 'undefined'
+              newdi['speech'] = elem[-1]
+              newdi['author_text'] = elem[0]
+            else:
+              newdi['author'] = 'undefined'
+              newdi['speech'] = elem[0]
+              newdi['author_text'] = elem[-1]
           elif type(elem) == tuple and len(elem) == 3:
             newdi['author'] = 'undefined'
             newdi['speech'] = elem[0] + ' ' + elem[-1]
             newdi['author_text'] = elem[1]
-          speechesdilist.append(newdi)
+          if newdi not in speechesdilist:
+            speechesdilist.append(newdi)
+        
         jsonspeeches['speeches'] = speechesdilist
         listforjson.append(jsonspeeches)
         counter += 1
@@ -103,4 +129,3 @@ def write_to_json(speech_and_comments):
       f.write(json.dumps(listforjson, ensure_ascii = False))
 
 write_to_json(delimited_speech)
-
